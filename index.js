b883e0b6055e12ae91347df60a341f46b0d37bd0
darvingaba <darvingaba1@gmail.com>
2022-06-13 16:52:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let salary = 500;
+let employeeContribution = (5 / 100) * 500;
+let taxableAmount = salary - employeeContribution;
+console.log("This is the taxable amount: " + taxableAmount);
+let tax = 0;
+if (salary <= 80) {
+    tax = 0;
+}else if (salary < 250 && salary > 80) {
+    tax = tax + (4 / 100) * taxableAmount;
+    console.log("Tax for salary bigger than 80 and lower than 250 " + tax);
+}else if (salary < 450 && salary > 250) {
+    tax = tax + (8 / 100) * taxableAmount;
+    console.log("Tax for salary bigger than 250 and lower than 450 " + tax);
+}else {
+    tax = tax + (10 / 100) * taxableAmount;
+    console.log("Tax for salary bigger than 450 " + tax);
+}
+let moneyIGet = taxableAmount - tax;
+console.log("This is what you get mf :" + moneyIGet);
