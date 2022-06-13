240e58a7a9df9e67b223853b6eb0176a713fa426
darvingaba <darvingaba1@gmail.com>
2022-06-13 17:03:08 +0200

    ..

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 let salary = 500;
 let employeeContribution = (5 / 100) * 500;
 let taxableAmount = salary - employeeContribution;
-console.log("This is the taxable amount: " + taxableAmount);
+console.log("This is the taxable amount: " + taxableAmount + " EUR");
 let tax = 0;
 if (salary <= 80) {
     tax = 0;
 }else if (salary < 250 && salary > 80) {
-    tax = tax + (4 / 100) * taxableAmount;
-    console.log("Tax for salary bigger than 80 and lower than 250 " + tax);
+    tax = tax + (4 / 100) * taxableAmount + "EUR";
+    console.log("Tax for salary bigger than 80 EUR and lower than 250 EUR: " + tax + " EUR");
 }else if (salary < 450 && salary > 250) {
     tax = tax + (8 / 100) * taxableAmount;
-    console.log("Tax for salary bigger than 250 and lower than 450 " + tax);
+    console.log("Tax for salary bigger than 250 and lower than 450 EUR: " + tax + " EUR");
 }else {
     tax = tax + (10 / 100) * taxableAmount;
-    console.log("Tax for salary bigger than 450 " + tax);
+    console.log("Tax for salary bigger than 450 EUR: " + tax + " EUR");
 }
 let moneyIGet = taxableAmount - tax;
-console.log("This is what you get :" + moneyIGet);
+console.log("This is what you get :" + moneyIGet + " EUR");
 
